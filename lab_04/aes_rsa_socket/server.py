f8972ed3b48d963ed20d289fd31d5e7df0fc9d30
NganPham-tech <phamthiaingan1205@gmail.com>
2025-06-09 13:48:30 +0700

    add

---
+++ lab_04/aes_rsa_socket/server.py
@@ -0,0 +1,10 @@
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import Pad, unpad
+import socket
+import threading
+import hashlib
+import threading
+
+
